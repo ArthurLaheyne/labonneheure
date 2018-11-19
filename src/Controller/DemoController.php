007c54904a61396a1de535ae6ce8bbf35816a59f
Arthur Laheyne <arthur.laheyne@gmail.com>
2018-11-19 22:08:05 +0100

    Bootstrap demo

--- src/Controller/DemoController.php
+++ src/Controller/DemoController.php
@@ -8,12 +8,20 @@ use Symfony\Component\Routing\Annotation\Route;
 class DemoController extends Controller
 {
     /**
-     * @Route("/", name="demo")
+     * @Route("/demo", name="demo")
      */
     public function index()
     {
         return $this->render('demo/index.html.twig', [
             'controller_name' => 'DemoController',
         ]);
+    }
+
+    /**
+     * @Route("/demo/bootstrap", name="demo_bootstrap")
+     */
+    public function bootstrap()
+    {
+        return $this->render('demo/bootstrap.html.twig');
     }
 }
